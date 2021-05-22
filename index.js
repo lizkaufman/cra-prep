5e7b43f4cab7083c718b03a8cc4b9bd75a412c8e
Liz Kaufman <lizkaufman92@gmail.com>
2021-05-22 16:15:22 +0100

    checks added to ensure this is run on a fresh cra app; probably too many but being safe rather than sorry!

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+const fs = require("fs");
+
 function prepFiles() {
-  // TODO: Check to make sure the correct files are where CRA puts them before running the helper functions to make sure that this isn't accidentally run on the wrong folder
+  if (!checkFiles()) {
+    console.log(
+      "This isn't a fresh CRA app. Remember to use cra-prep straight after you use create-react-app!"
+    );
+  }
+  console.log(
+    "Sweet, this is a fresh CRA app. Let's get started tidying it up so you can code! 😎"
+  );
   makeComponentFolders();
   moveAppFiles();
   emptyStarterCode();
@@ -8,6 +17,40 @@ function prepFiles() {
 prepFiles();
 
 //----- Helper functions: -----
+
+function checkFiles() {
+  const expectedCraFiles = [
+    ".git",
+    ".gitignore",
+    "README.md",
+    "node_modules",
+    "package-lock.json",
+    "package.json",
+    "public",
+    "src",
+  ];
+  const actualFiles = fs.readdirSync("./");
+  const fileCheck = expectedCraFiles.reduce((acc, cur) => {
+    if (!actualFiles.includes(cur)) {
+      return false;
+    }
+    return acc;
+  }, true);
+
+  const readmeCheck = fs
+    .readFileSync("./README.md", "utf-8")
+    .includes("# Getting Started with Create React App");
+
+  const srcFiles = fs.readdirSync("./src");
+  const noComponentsFolderCheck =
+    !srcFiles.map((file) => file.toLowerCase()).includes("components") &&
+    srcFiles.includes("App.js") &&
+    srcFiles.includes("logo.svg");
+
+  return [fileCheck, readmeCheck, noComponentsFolderCheck].every(
+    (check) => check === true
+  );
+}
 
 function makeComponentFolders() {
   // TODO: create components folder in ./src
